0807278ce6801cfb9cbe3a9263baf7c4fbebb3ea
Yan Brito <yan.briito@hotmail.com>
2021-11-08 22:35:12 -0300

    First commit

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { UserList } from './src/pages/UserList';
+
+export default function App() {
+  return(
+    <UserList/>
+  );
+};
---
+++ src/pages/UserList/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from './styles';
+
+export function UserList() {
+  return(
+    <Container>
+
+    </Container>
+  );
+};
